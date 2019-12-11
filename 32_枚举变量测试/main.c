3207dd79c3e90439881ab60dcccbd64ba34e628b
tanxiong <tan.xiong@wuqi-tech.com>
2019-12-11 16:29:23 +0800

    新增枚举变量测试

---
+++ 32_枚举变量测试/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+typedef enum {
+    MODEL_NORMAL = 0,
+    MODEL_WAKEUP,
+    MODEL_LOW_POWER,
+    MODEL_SLEEP
+} module_model;
+
+enum {
+    ACTION_SET = 0,
+    ACTION_SEND,
+    ACTION_REC
+} action_num;
+
+int main()
+{
+    uint32_t model = 0xff;
+    uint32_t act = 0xff;
+    printf("1: model:%d, act:%d \n",model,act);
+    model = MODEL_SLEEP;
+    act = ACTION_REC;
+    printf("2: model:%d, act:%d \n",model,act);
+    getchar();
+    return 0;
+}
